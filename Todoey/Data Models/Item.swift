1152cf167e2f091a055383f180f0e46dcf534df7
YerzhanA <e.assanoff@gmail.com>
2018-07-24 16:42:01 +0200

    Convert to MVC

---
+++ Todoey/Data Models/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Yerzhan Assanov on 12/7/18.
+//  Copyright © 2018 Yerzhan Assanov. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
